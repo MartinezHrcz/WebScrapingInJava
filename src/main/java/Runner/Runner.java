3cd40f0621d5522840e1a0a9002a880dbee9ba07
Herczeg Martinez <160034549+MartinezHrcz@users.noreply.github.com>
2025-03-24 12:52:00 +0100

    feat: The app now uses paralellism

--- src/main/java/Runner/Runner.java
+++ src/main/java/Runner/Runner.java
@@ -1,12 +1,16 @@
 package Runner;
 
 import Scraper.WebScraper;
+import Utils.DataWriter;
 import Utils.MenuCLI;
+import Utils.ProductUtils;
 
 public class Runner {
 
+    private static String url = "https://www.scrapingcourse.com/ecommerce";
     public static void main(String[] args) {
-        WebScraper.Scrape();
+        WebScraper.ParallelScraping(url);
         MenuCLI.StartMenu();
+
     }
 }
--- src/main/java/Scraper/WebScraper.java
+++ src/main/java/Scraper/WebScraper.java
@@ -8,42 +8,91 @@ import org.jsoup.select.*;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.*;
 
 public class WebScraper {
     public static ArrayList<Product> products = new ArrayList<>();
-
+    private static final int THREAD_POOL_SIZE = 10;
     private static Document page;
 
-    private static String url = "https://www.scrapingcourse.com/ecommerce";
-
-    public static void Scrape() {
 
 
-        while (url != null) {
-            try{
-                //Connecting to the site
-                page = Jsoup.connect(url)
-                        .userAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/134.0.0.0 Safari/537.36")
-                        .header("Accept-Language", "*").get();
-            }
-            catch (IOException e) {
-                System.out.println("Can't scrape " + url);
-            }
+    public static List<Product> ParallelScraping(String startURL) {
+        ExecutorService executor = Executors.newFixedThreadPool(THREAD_POOL_SIZE);
 
-            GatherData(page);
-            Element nextButton = page.selectFirst("a.next");
-            if (nextButton != null) {
-                String nextURL = nextButton.attr("href");
-                if (!nextURL.startsWith("http")) {
-                    nextURL = url + nextURL.replace("^/","");
+        //Thread safe variant of arrayList, add set... are implemented by making a fresh copy of the underlying array
+        List<Product> products = new CopyOnWriteArrayList<>();
+
+        //Future: interface which represents the result of an async computation
+        //Blocks until the computation is complete
+
+        List<Future<String>> futures = new ArrayList<>();
+        //to make sure a visited page isn't scraped more than once
+        ConcurrentSkipListSet<String> visitedPages = new ConcurrentSkipListSet<>();
+
+        //add the first page to the task queue
+        visitedPages.add(startURL);
+
+        futures.add((Future<String>) executor.submit(()-> Scrape(startURL,products,visitedPages)));
+
+        while (!futures.isEmpty())
+        {
+            List<Future<String>> newFutures = new ArrayList<>();
+            for (Future<String> future : futures)
+            {
+                try{
+                    //Waits for page scrape to complete
+                    String nextUrl = future.get();
+                    if (nextUrl != null && visitedPages.add(nextUrl)){
+                        newFutures.add((Future<String>) executor.submit(()-> Scrape(nextUrl,products,visitedPages)));
+                    }
                 }
-                url = nextURL;
+                catch (ExecutionException | InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+
             }
-            else {
-                url = null;
-            }
+            futures = newFutures;
         }
 
+        executor.shutdown();
+        try{
+            executor.awaitTermination(60,TimeUnit.SECONDS);
+        }
+        catch (InterruptedException e) {
+            System.out.println("Execution interrupted:" + e.getMessage());
+        }
+
+        return products;
+    }
+
+    public static void Scrape(String startURL, List<Product> products, ConcurrentSkipListSet<String> visitedPages) {
+        long Start = System.nanoTime();
+        try{
+            //Connecting to the site
+            page = Jsoup.connect(startURL)
+                    .userAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/134.0.0.0 Safari/537.36")
+                    .header("Accept-Language", "*").get();
+        }
+        catch (IOException e) {
+            System.out.println("Can't scrape " + startURL);
+        }
+
+        GatherData(page);
+        Element nextButton = page.selectFirst("a.next");
+        if (nextButton != null) {
+            String nextURL = nextButton.attr("href");
+            if (!nextURL.startsWith("http")) {
+                nextURL = startURL + nextURL.replace("^/","");
+            }
+            startURL = nextURL;
+        }
+        else {
+            startURL = null;
+        }
+        long End = System.nanoTime();
+        System.out.println("Took: " + (End - Start) / 1000000 + "ms");
 
     }
 
